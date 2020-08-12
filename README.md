8664bec554752ede6a0feb5e43502f0edd54c1db
Erik <erik.leven@sesam.io>
2020-08-12 10:03:55 +0200

    updated the MS to support both REST versions 2 and 3. Removed dotdictify. Updated README

--- README.md
+++ README.md
@@ -1,36 +1,231 @@
-# Catalystone Rest
+"#" Catalystone Rest
 [![Build Status](https://travis-ci.org/sesam-community/catalystone-rest.svg?branch=master)](https://travis-ci.org/sesam-community/catalystone-rest)
 
-A microservice to connect to catalystones rest interface.
+A microservice to connect to catalystones REST interface for REST versions 2 and 3. Both api versions can be used with this microservice, but read through the examples on how to set them up.
 
 It gives the options of having connections to several different endpoints, catalystone have different tokens and secrets for each of them so specify this in ENV-vars and secrets
 
-##### Example configuration:
+
+## Environment variables
+
+`client_id_org` - The client ID for loading organization entities into Sesam. This is only applicable in REST version 2 at the moment as REST version 3 does not support organization data.
+
+`client_secret_org` - The client secret associated with `client_id_org`.
+
+`client_id_user` - The client ID for loading user data into Sesan. This is supported by both REST version 2 and 3. 
+
+`client_secret_user` - The client secret associated with client_id_user.
+
+`get_url` - The root API URL address associated with REST version 2. This is the same for both organizations and users.
+
+`employee_url` - The root API URL address associated with REST version 3.   
+
+`grant_type` - The grant type for API calls.
+
+`token_url` - The URL for acquiring an access token for the different REST versions
+
+`api_version` - The version of the REST required. Default is 'v2'. 
+
+## Example system configuration for v2:
 
 
 ```
 {
-  "_id": "catalystone-rest",
+  "_id": "catalystone-rest-v2",
   "type": "system:microservice",
   "docker": {
     "environment": {
-      "client_id_org": "$ENV(client_id_org)",
-      "client_id_post": "$ENV(client_id_post)",
-      "client_id_user": "$ENV(client_id_user)",
-      "client_secret_org": "$SECRET(client_secret_org)",
-      "client_secret_post": "$SECRET(client_secret_post)",
-      "client_secret_user": "$SECRET(client_secret_user)",
-      "entities_path_org": "ORGANIZATIONS.ORGANIZATION",
-      "entities_path_user": "USERS.USER",
-      "get_url": "URL_TO_GET",
+      "client_id_org": "$ENV(catalystone-v2-client_id_org)",
+      "client_id_user": "$ENV(catalystone-v2-client_id_user)",
+      "client_secret_org": "$SECRET(catalystone-v2-client_secret_org)",
+      "client_secret_user": "$SECRET(catalystone-v2-client_secret_user)",
+      "get_url": "https://bt.catalystone.com/bouvet/api/v2/export",
       "grant_type": "client_credentials",
-      "post_headers": "{'content_type': 'application/json', 'Accept':'application/json'}",
-      "post_url": "URL_TO_POST",
-      "token_url": "URL_TO_TOKEN"
+      "token_url": "https://bt.catalystone.com/bouvet/api/v2/oauth2/token"
     },
-    "image": "sesamcommunity/catalystone-rest:latest",
+    "image": "sesamcommunity/catalystone-rest:2.0.0",
     "port": 5000
   }
 }
 
+
 ```
+
+## Example pipe configuration for v2:
+# This pipe loads users into Sesam:
+
+```
+{
+  "_id": "catalystone-user",
+  "type": "pipe",
+  "source": {
+    "type": "json",
+    "system": "catalystone-rest-v2",
+    "url": "user"
+  },
+  "sink": {
+    "type": "dataset",
+    "dataset": "catalystone-user"
+  },
+  "transform": {
+    "type": "dtl",
+    "rules": {
+      "default": [
+        ["add", "_id",
+          ["string", "_S.STANDARD_FIELDS.UNIQUE_IMPORT_ID.value"]
+        ],
+        ["copy", "*"]
+      ]
+    }
+  }
+}
+
+
+```
+# This pipe loads organisations into Sesam:
+```
+
+{
+  "_id": "catalystone-organization",
+  "type": "pipe",
+  "source": {
+    "type": "json",
+    "system": "catalystone-rest-v2",
+    "url": "organization"
+  },
+  "transform": {
+    "type": "dtl",
+    "rules": {
+      "default": [
+        ["copy", "*"]
+      ]
+    }
+  }
+}
+
+```
+
+
+## Example system configuration for v3:
+
+
+```
+{
+  "_id": "catalystone-rest-v3",
+  "type": "system:microservice",
+  "docker": {
+    "environment": {
+      "api_version": "v3",
+      "client_id_user": "$ENV(catalystone-v3-client_id_user)",
+      "client_secret_user": "$SECRET(catalystone-v3-client_secret_user)",
+      "employee_url": "https://bt.catalystone.com/bouvet/api/employees",
+      "grant_type": "client_credentials",
+      "token_url": "https://bt.catalystone.com/bouvet/api/accesstoken"
+    },
+    "image": "sesamcommunity/catalystone-rest:2.0.0",
+    "port": 5000
+  }
+}
+
+
+```
+
+## Example pipe configurations for v3:
+# This pipe loads users into Sesam:
+
+```
+
+{
+  "_id": "catalystone-user",
+  "type": "pipe",
+  "source": {
+    "type": "json",
+    "system": "catalystone-rest-v3",
+    "url": "employees"
+  },
+  "transform": {
+    "type": "dtl",
+    "rules": {
+      "default": [
+        ["add", "_id", "_S.employeeId"],
+        ["copy", "*"]
+      ]
+    }
+  }
+}
+
+```
+
+# This pipe creates the payloads for field numbers 101 and 7 to post to users for v3:
+
+```
+{
+  "_id": "user-catalystone",
+  "type": "pipe",
+  "source": {
+    "type": "dataset",
+    "dataset": "global-person"
+  },
+  "transform": {
+    "type": "dtl",
+    "rules": {
+      "default": [
+        ["add", "employees",
+          ["list",
+            ["dict", "guid", "_S.guid", "field",
+              ["dict", "101",
+                ["dict", "data",
+                  ["dict", "value",
+                    ["first", "<some value>"]
+                  ]
+                ], "7",
+                ["dict", "data",
+                  ["dict", "value",
+                    ["first", "<some value>"]
+                  ]
+                ]
+              ]
+            ]
+          ]
+        ]
+      ]
+    }
+  }
+}
+
+```
+
+
+# This pipe posts users for v3:
+
+```
+{
+  "_id": "user-catalystone-endpoint",
+  "type": "pipe",
+  "source": {
+    "type": "dataset",
+    "dataset": "user-catalystone"
+  },
+  "sink": {
+    "type": "json",
+    "system": "catalystone-rest-v3",
+    "url": "/post_employees"
+  },
+  "transform": {
+    "type": "dtl",
+    "rules": {
+      "default": [
+        ["filter",
+          ["eq", "_S._deleted", false]
+        ],
+        ["copy", "*"],
+        ["remove", "_*"]
+      ]
+    }
+  }
+}
+
+
+```
+
+The catalystone-user pipe for version 3 can also utilize queries when loading user data. To specify a query, put "employees;<query>" in the url tag in the pipe config. To see what queries are available, please read the catalystone version 3.0 API documentation. 
--- service/catalystone-rest.py
+++ service/catalystone-rest.py
@@ -4,7 +4,6 @@ import requests
 import logging
 import sys
 import json
-import dotdictify
 
 app = Flask(__name__)
 logger = None
@@ -12,18 +11,22 @@ format_string = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 logger = logging.getLogger('catalystone-service')
 
 # Log to stdout
-
 stdout_handler = logging.StreamHandler()
 stdout_handler.setFormatter(logging.Formatter(format_string))
 logger.addHandler(stdout_handler)
 logger.setLevel(logging.DEBUG)
 
-##getting token
 def get_token(path):
     headers = {}
     logger.info("Creating header")
-
-    if path == "user":
+    if path == "employees" or path == "post_employees":
+        headers = {
+            "Client-Id":os.environ.get('client_id_user'),
+            "Client-Secret":os.environ.get('client_secret_user'),
+            "Grant-Type":os.environ.get('grant_type'),
+            "Api-Version": os.environ.get('api_version')
+        }
+    elif path == "user":
         headers = {
             "client_id":os.environ.get('client_id_user'),
             "client_secret":os.environ.get('client_secret_user'),
@@ -35,77 +38,90 @@ def get_token(path):
             "client_secret":os.environ.get('client_secret_org'),
             "grant_type":os.environ.get('grant_type')
         }
-    elif path == "post_user":
-        headers = {
-            "client_id": os.environ.get('client_id_post'),
-            "client_secret": os.environ.get('client_secret_post'),
-            "grant_type": os.environ.get('grant_type')
-        }
     else:
-        logger.info("undefined method")
+        logger.info("undefined path")
         sys.exit()
+    resp = requests.get(url=os.environ.get('token_url'), headers=headers)
+    if resp.status_code != 200:
+        logger.error("Unexpected response status code: %d with response text %s" % (resp.status_code, resp.text))
+        raise AssertionError ("Unexpected response status code: %d with response text %s"%(resp.status_code, resp.text))
 
-    resp = requests.get(url=os.environ.get('token_url'), headers=headers).json()
-    token = dotdictify.dotdictify(resp).response.responseMessage.access_token
+    if path == "employees" or path == "post_employees":
+        token = resp.json()["access_token"]
+
+    else:
+        token = resp.json()["response"]["responseMessage"]["access_token"]
+
     logger.info("Received access token from " + os.environ.get('token_url'))
     return token
 
 class DataAccess:
 
 #main get function check for path and make decisions based on that value
-    def __get_all_entities(self, path):
-        logger.info("Fetching data from url: %s", path)
+    def __get_all_entities(self, path, query, employeenr):
+        if query:
+            logger.info("Using query: %s", query)
         token = get_token(path)
-        headers= {'Accept': 'application/json',
-                  'content_type': 'application/json'}
-        url = os.environ.get('get_url') + "?access_token=" + token
-        req = requests.get(url, headers=headers)
+
+        if path == "employees":
+            headers= {"Access-Token": token, 
+                      "Api-Version": os.environ.get('api_version')}
+            if not query:
+                if not employeenr:
+                    url = os.environ.get('employee_url')
+                else:
+                    url = os.environ.get('employee_url') + "/" + employeenr
+            else:
+                url = os.environ.get('employee_url') + query
+            logger.info("url = " + url)
+        else:
+            headers= {'Accept': 'application/json',
+                      'content_type': 'application/json'}
+            url = os.environ.get('get_url') + "?access_token=" + token
+
+        req = requests.get(url=url, headers=headers)
 
         if req.status_code != 200:
             logger.error("Unexpected response status code: %d with response text %s" % (req.status_code, req.text))
             raise AssertionError ("Unexpected response status code: %d with response text %s"%(req.status_code, req.text))
-        res = dotdictify.dotdictify(json.loads(req.text))
+        res = req.json()
         if path == "user":
-            for entity in res.get(os.environ.get("entities_path_user")):
-
+            for entity in res["USERS"]["USER"]:
                 yield(entity)
-        if path == "organization":
-            for entity in res.get(os.environ.get("entities_path_org")):
-                yield (entity)
+        elif path == "employees":
+            for entity in res["employees"]:
+                yield(entity)
+        elif path == "organization":
+            for entity in res["ORGANIZATIONS"]["ORGANIZATION"]:
+                yield(entity)
         else:
             logger.info("method not recognized")
         logger.info('Returning entities from %s', path)
 
-    def get_entities(self,path):
+    def get_entities(self,path, query, employeenr):
         print("getting all")
-        return self.__get_all_entities(path)
+        return self.__get_all_entities(path, query, employeenr)
 
 data_access_layer = DataAccess()
 
 
-def update_entities(entities, headers, post_url, counter):
+def update_entities(entities, headers, counter):
     if counter == 0:
-        #for i in range (total_list):
         for entity in entities:
-
             entity.pop('_id', None)
-            response = requests.post(post_url, data=json.dumps(entity), headers=headers)
+            response = requests.put(os.environ.get('employee_url'), data=json.dumps(entity), headers=headers)
             if response.status_code is not 200:
                 if response.status_code == 403:
-                    logger.info('stuff happens')
                     logger.error("Unexpected response status code: %d with response text %s" % (response.status_code, response.text) + str(counter))
                     raise AssertionError("Unexpected response status code: %d with response text %s" % (response.status_code, response.text) + str(counter))
                 logger.error("Got error code: " + str(response.status_code) + " with text: " + response.text + str(counter))
                 return Response(response.text, status=response.status_code, mimetype='application/json')
-            logger.info("Processed " + entity['USERS']['USER'][0]['STANDARD_FIELDS']['UNIQUE_IMPORT_ID'])
             counter +=1
         return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
 
     else:
         counter = counter
         for entity in entities[counter:]:
-        #for entity in entities and counter < total_list:
-
             entity.pop('_id', None)
             response = requests.post(post_url, data=json.dumps(entity), headers=headers)
             if response.status_code is not 200:
@@ -114,7 +130,6 @@ def update_entities(entities, headers, post_url, counter):
                     raise AssertionError("Unexpected response status code: %d with response text %s" % (response.status_code, response.text), +str(counter))
                 logger.error("Got error code: " + str(response.status_code) + " with text: " + response.text + str(counter))
                 return Response(response.text, status=response.status_code, mimetype='application/json')
-            logger.info("Processed " + entity['USERS']['USER'][0]['STANDARD_FIELDS']['UNIQUE_IMPORT_ID'])
             counter +=1
         return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
 
@@ -132,33 +147,47 @@ def stream_json(clean):
 
 @app.route("/<path:path>", methods=["GET", "POST"])
 def get_path(path):
+    try:    
+        s = path.split(";")
+        path = s[0]
+        query = "?" + s[1]
+    except IndexError:
+        path = path
+        query = False
+    try: 
+        s = path.split("/")
+        path = s[0]
+        employeenr = s[1]
+    except IndexError:
+        path = path
+        employeenr = False
+
+    if employeenr != False and query != False:
+        logger.error("API does not accept both User identifier field and query. Choose one or the other..")
 
     if request.method == 'POST':
-        post_url = os.environ.get('post_url') + "?access_token=" + get_token(path)
+        access_token = get_token(path)
         entities = request.get_json()
         counter = 0
-        headers = json.loads(os.environ.get('post_headers').replace("'", "\""))
+        headers = {"Access-Token": access_token, "Api-Version": os.environ.get('api_version'), "Content-Type": "application/json"}
         logger.info("Sending entities")
         try:
-            return update_entities(entities, headers, post_url, counter)
+            return update_entities(entities, headers, counter)
 
         except Exception as e:
             logger.info(e)
             counter = int(e.args[0].split('}')[-1])
-            post_url = os.environ.get('post_url') + "?access_token=" + get_token(path)
-            return update_entities(entities, headers, post_url, counter)
-
+            return update_entities(entities, headers, counter)
 
     elif request.method == "GET":
-        path = path
-        entities = data_access_layer.get_entities(path)
+        entities = data_access_layer.get_entities(path, query, employeenr)
         return Response(
             stream_json(entities),
             mimetype='application/json'
         )
-
     else:
         logger.info("undefined request method")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', threaded=True, port=os.environ.get('port',5000))
+
